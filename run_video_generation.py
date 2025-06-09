c4ecb51b1369364f447c9499a47043e0bd4173a3
Amir Othman <1488224+amirothman@users.noreply.github.com>
2025-06-09 21:17:38 +0200

    add image

--- run_video_generation.py
+++ run_video_generation.py
@@ -1,8 +1,6 @@
 r"""Example.
 
-curl -X POST   -d '{"prompt": "Stormcaller dancer in charged silk summons lightning with fan flourishes, mountain peak tempest, time-lapse cloud movement, sumi-e brushwo
-rk", "width":832, "height":480}'   -H "Authorization: bearer token-here"   -H 'Content-Type: application/json'   'https://api.deepinfra.com/v1/inference/black-forest-labs/FLUX-1-schn
-ell' | jq -r '.images[0]' | sed 's/^data:image\/png;base64,//' | base64 -d > schnell-output.png
+curl -X POST   -d '{"prompt": "Stormcaller dancer in charged silk summons lightning with fan flourishes, mountain peak tempest, time-lapse cloud movement, sumi-e brushwork", "width":832, "height":480}'   -H "Authorization: bearer token-here"   -H 'Content-Type: application/json'   'https://api.deepinfra.com/v1/inference/black-forest-labs/FLUX-1-schnell' | jq -r '.images[0]' | sed 's/^data:image\/png;base64,//' | base64 -d > schnell-output.png
 
 python create_sample_video.py --prompt "Stormcaller dancer in charged silk summons lightning with fan flourishes, mountain peak tempest, time-lapse cloud movement, sumi-
 e brushwork" --video_length 120
